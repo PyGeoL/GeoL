db96ad1ef057bbebf5d18dadef25537938172457
Michele Ferretti <mic.ferretti@gmail.com>
2018-03-07 15:20:58 +0100

    update var name

--- scripts/SPTK_matrix.py
+++ scripts/SPTK_matrix.py
@@ -73,7 +73,7 @@ def main(argv):
     if m:
         size = m.group(1)
 
-    m = re.search('.(.+).model', args.input_file)
+    m = re.search('.(.+).model', args.inputfile)
     if m:
         model_details = m.group(1)
 
