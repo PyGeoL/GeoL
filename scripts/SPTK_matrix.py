4bacd8d450d6c4ec5517f20bab1c40ae9d89a2de
Michele Ferretti <mic.ferretti@gmail.com>
2018-03-12 10:48:49 +0100

    accept gnews model

--- scripts/SPTK_matrix.py
+++ scripts/SPTK_matrix.py
@@ -69,7 +69,6 @@ def main(argv):
     if(args.inputfile.endswith(ext)):
         model = gensim.models.KeyedVectors.load_word2vec_format(
             args.inputfile, binary=True)
-
     else:
         model = gensim.models.Word2Vec.load(args.inputfile)
 
@@ -85,9 +84,12 @@ def main(argv):
     if m:
         size = m.group(1)
 
-        m = re.search('.+/(.+).model', args.inputfile)
+    m = re.search('.+/(.+).model', args.inputfile)
+
     if m:
         model_details = m.group(1)
+    else:
+        model_details = 'gnews'
 
     outputfile = os.path.abspath(os.path.join(
         args.outputfolder, "matrix_" + args.area + "_" + model_details + ".txt"))
